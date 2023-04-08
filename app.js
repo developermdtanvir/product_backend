c16113d433e508a19fedbdfebd08a676d10d76cf
mdtanvirgit <officialtanvir@hotmail.com>
2023-04-08 15:25:09 +0600

    my product backend find && findOne is ready

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+
+
+const port = process.env.PORT || 5000
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.338egrb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+
+        const db = client.db('food_collection');
+        const foodCollection = db.collection('food');
+
+
+        app.get('/foods', async (req, res) => {
+            const query = {};
+            const cursor = await foodCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.findOne(query);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('My product collection project is running')
+})
+
+app.listen(port, () => console.log(`listening port ${port}`))
